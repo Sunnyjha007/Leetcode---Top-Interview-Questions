fa580c026f76a62f6e301223a311d8018071a1d0
sunny jha <78363637+Sunnyjha007@users.noreply.github.com>
2022-08-09 13:22:10 +0530

    Create 28. Implement strStr().cpp

---
+++ 28. Implement strStr().cpp
@@ -0,0 +1,18 @@
+int strStr(string haystack, string needle) {
+        int m = haystack.size();
+        int n = needle.size();
+        if(n==0)return 0;
+        int j = 0;
+        for(int i=0;i<m;i++){
+            if(haystack[i] == needle[j]){
+                j++;
+            }else{
+                i = i-j;
+                j=0;
+            }
+            if(j==n){
+                return i-j+1;
+            }
+        }
+        return -1;
+    }
