2a193317d668dda474cb2550e8bb57f29391443d
sunny jha <78363637+Sunnyjha007@users.noreply.github.com>
2022-08-11 04:22:15 +0530

    Create 31. Next Permutation.cpp

---
+++ 31. Next Permutation.cpp
@@ -0,0 +1,11 @@
+void nextPermutation(vector<int>& nums) {
+       int n = nums.size();
+        int i=n-1;
+        while(i>0&&nums[i-1]>=nums[i])i--;
+        if(i==0){reverse(nums.begin(),nums.end());return;}
+        int j = n-1;
+        while(j>0&&nums[i-1]>=nums[j])j--;
+        swap(nums[i-1],nums[j]);
+        reverse(nums.begin()+i,nums.end());
+        return;
+    }
